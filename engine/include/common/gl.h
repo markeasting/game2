80d685899394fa3d78a3fd8398fb9bd14c1ad912
Mark Oosting <markeasting@gmail.com>
2025-07-14 22:44:10 +0200

    Added GL error check macros

--- engine/include/common/gl.h
+++ engine/include/common/gl.h
@@ -6,7 +6,23 @@
     do { \
         GLenum error = glGetError(); \
         if (error != GL_NO_ERROR) { \
-            printf("[OpenGL Error] Code: %d\n", error); \
-            throw std::runtime_error("OpenGL error occurred."); \
+            printf("[OpenGL ERR] Code: %d, File: %s:%d\n", \
+                error, __FILE__, __LINE__); \
         } \
+    } while (0)
+
+#define CHECK_GL_ERROR_THROW() \
+    do { \
+        GLenum error = glGetError(); \
+        if (error != GL_NO_ERROR) { \
+            throw std::runtime_error( \
+                std::string("[OpenGL ERR] Code: ") + std::to_string(error) + \
+                ", File: " + __FILE__ + ":" + std::to_string(__LINE__) \
+            ); \
+        } \
+    } while (0)
+
+#define GL_CLEAR_ERRORS() \
+    do { \
+        while (glGetError() != GL_NO_ERROR); \
     } while (0)
